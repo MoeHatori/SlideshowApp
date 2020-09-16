3085674d82383fb7a1f7dc7c38f30a5bf7804f8e
moe.hatori <moe.hatori@jreast.co.jp>
2020-09-16 14:25:25 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Chan Yama on 2020/09/16.
+//  Copyright © 2020 moe.hatori2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
